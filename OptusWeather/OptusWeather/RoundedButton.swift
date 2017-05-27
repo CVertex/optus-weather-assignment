b077f7c319c9f7de7047723f855bc0873819e9aa
Vijay Santhanam <vijay.santhanam@gmail.com>
2017-05-27 18:32:29 +1000

    Improve styles of Error and Loading views
    Rounded buttons are nice :)

---
+++ OptusWeather/OptusWeather/RoundedButton.swift
@@ -0,0 +1,35 @@
+//
+//  RoundedButton.swift
+//  OptusWeather
+//
+//  Created by Vijay Santhanam on 27/05/2017.
+//  Copyright © 2017 Vijay Santhanam. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@IBDesignable public class RoundedButton : UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = .clear
+    }
+}
