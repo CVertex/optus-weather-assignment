e0f337c35ade687ebe40086366805d71a812e4cd
Vijay Santhanam <vijay.santhanam@gmail.com>
2017-05-28 23:08:56 +1000

    Add NavController overrides for rotation

---
+++ OptusWeather/OptusWeather/UINavigationControllerRotation.swift
@@ -0,0 +1,24 @@
+//
+//  UINavigationControllerRotation.swift
+//  OptusWeather
+//
+//  Created by Vijay Santhanam on 28/05/2017.
+//  Copyright © 2017 Vijay Santhanam. All rights reserved.
+//
+
+import UIKit
+
+// Disable rotation 
+extension UINavigationController {
+    
+    open override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+}
+
