a64abb7c042447c47a5e6e4ca53f8096bdfba851
Vijay Santhanam <vijay.santhanam@gmail.com>
2017-05-31 15:39:27 +1000

    linted

--- OptusWeather/OptusWeather/DebugOverlay.swift
+++ OptusWeather/OptusWeather/DebugOverlay.swift
@@ -5,18 +5,16 @@
 //  Created by Vijay Santhanam on 31/05/2017.
 //  Copyright © 2017 Vijay Santhanam. All rights reserved.
 //
-
 import UIKit
 
 /// Manager for Apple's private Debugging Overlay
 public class DebugOverlay {
-    
     /// Singleton
     public static let shared = DebugOverlay()
 
     // Overlay
     var overlayClass: UIWindow.Type? = nil
-    
+
     /// Prepares a private iOS debugging overlay
     /// See http://ryanipete.com/blog/ios/swift/objective-c/uidebugginginformationoverlay/
     /// Call on app boot
@@ -24,10 +22,11 @@ public class DebugOverlay {
         self.overlayClass = NSClassFromString("UIDebuggingInformationOverlay") as? UIWindow.Type
         _ = overlayClass?.perform(NSSelectorFromString("prepareDebuggingOverlay"))
     }
-    
+
     /// Toggle overlay visiblity
     public func toggle() {
-        let overlay = overlayClass?.perform(NSSelectorFromString("overlay")).takeUnretainedValue() as? UIWindow
+        let overlaySel = NSSelectorFromString("overlay")
+        let overlay = overlayClass?.perform(overlaySel).takeUnretainedValue() as? UIWindow
         _ = overlay?.perform(NSSelectorFromString("toggleVisibility"))
     }
 }
