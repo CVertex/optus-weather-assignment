5d712ae1cabed0f4d5962abac070396fc4b2c29a
Vijay Santhanam <vijay.santhanam@gmail.com>
2017-06-07 22:52:54 +1000

    Refactor formatters

--- OptusWeather/OptusWeather/Formatters.swift
+++ OptusWeather/OptusWeather/Formatters.swift
@@ -23,53 +23,64 @@ public struct Formatters {
     }()
 
     // MARK: Temperature
-    public static func tempToString(value: Float) -> String {
-        return "\(temperature.string(from: NSNumber(value:value))!)°"
-    }
+    public struct Temp {
+        public static func string(from value: Float) -> String {
+            return "\(formatter.string(from: NSNumber(value:value))!)°"
+        }
 
-    public static let temperature: NumberFormatter = {
-        let nf = NumberFormatter()
-        nf.numberStyle = .decimal
-        nf.maximumFractionDigits = 1
-        return nf
-    }()
+        public static let formatter: NumberFormatter = {
+            let nf = NumberFormatter()
+            nf.numberStyle = .decimal
+            nf.maximumFractionDigits = 1
+            return nf
+        }()
+    }
 
     // MARK: Distance
-    public static func distanceToString(meters: Float) -> String {
-        return "\(distance.string(from: NSNumber(value:meters))!)m"
-    }
+    public struct Distance {
+        public static func string(from meters: Float) -> String {
+            return "\(formatter.string(from: NSNumber(value:meters))!)m"
+        }
 
-    public static let distance: NumberFormatter = {
-        let f = NumberFormatter()
-        f.numberStyle = .decimal
-        return f
-    }()
+        public static let formatter: NumberFormatter = {
+            let f = NumberFormatter()
+            f.numberStyle = .decimal
+            return f
+        }()
+    }
 
     // MARK: Percentage
-    public static func percentageToString(value: Float) -> String {
-        return "\(percentage.string(from: NSNumber(value:value))!)%"
+    public struct Percentage {
+        public static func string(from value: Float) -> String {
+            return "\(formatter.string(from: NSNumber(value:value))!)%"
+        }
+
+        public static let formatter: NumberFormatter = {
+            let f = NumberFormatter()
+            f.numberStyle = .decimal
+            f.maximumFractionDigits = 0
+            return f
+        }()
     }
 
-    public static let percentage: NumberFormatter = {
-        let f = NumberFormatter()
-        f.numberStyle = .decimal
-        f.maximumFractionDigits = 0
-        return f
-    }()
-
-    public static func rainVolumeToString(value: Float) -> String {
-        return "\(percentage.string(from: NSNumber(value:value))!)mm"
+    // MARK: Rain Volume
+    public struct RainVolume {
+        public static func string(from value: Float) -> String {
+            return "\(Percentage.formatter.string(from: NSNumber(value:value))!)mm"
+        }
     }
 
     // MARK: Pressure
-    public static func pressureToString(value: Float) -> String {
-        return "\(pressure.string(from: NSNumber(value:value))!)hPa"
-    }
+    public struct Pressure {
+        public static func string(from value: Float) -> String {
+            return "\(formatter.string(from: NSNumber(value:value))!)hPa"
+        }
 
-    public static let pressure: NumberFormatter = {
-        let f = NumberFormatter()
-        f.numberStyle = .decimal
-        f.maximumFractionDigits = 0
-        return f
-    }()
+        public static let formatter: NumberFormatter = {
+            let f = NumberFormatter()
+            f.numberStyle = .decimal
+            f.maximumFractionDigits = 0
+            return f
+        }()
+    }
 }
